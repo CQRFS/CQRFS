ea039e59fc4d344cfff321c6d8f5a92060d656f1
frankyymartian <fengguotian@dhcp020-195-147.wireless.buffalo.edu>
2015-11-30 00:22:10 -0500

    First commit

---
+++ posting/dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+if(!mysql_connect("localhost","root","tx19900101"))
+{
+	///////cqrfs fgt
+	// Database: `user_profile`
+// Table structure for table `user_reg`
+	die('Error  cannot build connection ! --> '.mysql_error());
+}
+if(!mysql_select_db("posting"))
+{
+	die('Error database selection problem ! --> '.mysql_error());
+}
+else 
+{
+	echo "success";
+}
+?>
