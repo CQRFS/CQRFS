7df26b8899ecf54c1c66d85a33591c76f1deaf71
frankyyyt <frankyyyt@gmail.com>
2015-11-30 01:49:02 -0500

    Update search.php

--- search/search/search.php
+++ search/search/search.php
@@ -3,7 +3,8 @@
     $con=mysql_connect("localhost","root","3317323") or die("Error connecting to database: ".mysql_error());
     /*
         localhost - it's location of the mysql server, usually localhost
-        root - your username
+        root - your username //Fengguo tian editing
+
         third is your password
          
         if connection fails it will stop loading the page and display an error
